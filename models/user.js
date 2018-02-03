dca968636cbbee53eaed00d1eb7dd0f97411656c
Jason Snider <jason@jasonsnider.com>
2018-02-03 16:34:42 -0600

    Add a user schema

---
+++ models/user.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  uniqueValidator = require('mongoose-unique-validator');
+
+//Create a schema
+var User = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: [true, 'Email is already in use']
+  },
+  username: {
+    type: String,
+    required: [true, 'Please enter a username'],
+    unique: [true, 'Username is already in use']
+  },
+  first_name: String,
+  last_name: String,
+  admin: {
+    type: Boolean,
+    default: false
+  }
+});
+
+User.plugin(uniqueValidator);
+
+module.exports  = mongoose.model('User', User);
