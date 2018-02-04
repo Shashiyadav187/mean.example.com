a788806f4fade23968121572b6ac9a9bdf7c74ea
Jason Snider <jason@jasonsnider.com>
2018-02-03 20:02:48 -0600

    GET a single user by id

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 var User = require('../../models/user');
 
 /* GET users listing. */
@@ -12,6 +13,23 @@ router.get('/', function(req, res){
     }
 
     return res.json({'success':true, 'users': users});
+
+  });
+
+});
+
+/* GET a single user. */
+router.get('/:userId', function(req,res){
+  
+  var userId = req.params.userId;
+
+  User.findOne({'_id':userId}, function(err, user){
+
+    if(err){
+      return res.json({'success':false, 'error': err});
+    }
+
+    return res.json({'success':true, 'user': user});
 
   });
 
