2221d9e27307816a0e5ffd82c1f6f06d92ca8b4f
Jason Snider <jason@jasonsnider.com>
2018-02-09 19:45:27 -0600

    Create the Article schema

---
+++ models/article.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  uniqueValidator = require('mongoose-unique-validator');
+
+//Create a schema
+var Article = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Please enter a title'],
+    unique: [true, 'Title is already in use']
+  },
+  slug: {
+    type: String,
+    required: [true, 'Please enter a slug'],
+    unique: [true, 'Slug is already in use']
+  },
+  keywords: String,
+  description: String,
+  body: String,
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  modified: {
+    type: Date,
+    default: Date.now
+  },
+  published: {
+    type: Date,
+    required: [true, 'Please enter a pub date'],
+  }
+});
+
+//Auto set the modified date on save
+Article.pre('save', function(next){
+  this.modified = new Date().toISOString();
+  next();
+});
+
+Article.plugin(uniqueValidator);
+
+module.exports  = mongoose.model('Article', Article);
